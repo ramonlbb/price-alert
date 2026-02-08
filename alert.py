97127c88d4b48a7bb7da50ae39ff46a536a1cb70
Ramon Lucas <128474790+ramonlbb@users.noreply.github.com>
2026-02-07 23:13:23 -0300

    Refactor Telegram token and chat ID environment variables
    Updated environment variable names for Telegram token and chat ID.

--- alert.py
+++ alert.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-TELEGRAM_TOKEN = os.getenv("8144439282:AAERv54Al04XeVVXoYGSsb80KyvB3jOPj0I")
-CHAT_ID = os.getenv("957507608")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
